24bd1c0089c268bc7713b85c707adcd1c8411bb1
hosin <didghtls11@gmail.com>
2024-06-29 16:56:23 +0900

    [Refactor] 카테고리 기능 개선
    - pinia로 category 스토어를 생성하여 상태를 관리할 수 있게 구성
    - 상품 등록 중에서 카테고리 데이터 변경
    - 카테고리를 클릭할 시 view-router로 넘어가는 부분 수정

---
+++ src/stores/useCategoryStore.js
@@ -0,0 +1,30 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+import { useProductStore } from "@/stores/useProductStore";
+
+const backend = process.env.VUE_APP_ENDPOINT
+
+export const useCategoryStore = defineStore('category', {
+    state: () => ({
+        productListWithCategory: []
+    }),
+    actions: {
+        async getProductListWithCategory(categoryName) {
+            let token = localStorage.getItem('accessToken')
+            let response = await axios.get(backend + "/category/search/type/" + categoryName, {
+                headers: {
+                    Authorization: token
+                }
+            })
+
+            if (this.productListWithCategory.length > 0) {
+                this.productListWithCategory = []
+            }
+            response.data.result.forEach((product) => {
+                this.productListWithCategory.push(product)
+            })
+
+            useProductStore().productTest(this.productListWithCategory)
+        }
+    }
+})
